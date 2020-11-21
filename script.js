527463fb64357e1bea17f5dfc8634716d0562258
Nardin Lachowski <nardin@live.com>
2020-11-21 09:12:13 -0500

    timer to start when user click Start Quiz

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 // Time counter variables/ end of quiz
 var timeEl = document.querySelector (".time");
 var mainEl = document.querySelector ("h2");
+var quizContainer = document.querySelector("#quizQuestion");
+var submitAnswer = document.querySelector("button");
+var startTimer = document.querySelector(".start-button");
 
+// declare time user has to finish quiz
 var secondsLeft = 5;
 
-
+// timer logic
 function setTime() {
     var timeInterval = setInterval (function() {
         secondsLeft--;
@@ -25,7 +29,18 @@ function sendMessage() {
 
 }
 
-setTime();
+//setTime(); /to test timer runs on page loads
+// eventlister function to start timer when "start quiz" button is clicked
+startTimer.addEventListener('click', setTime);
+
+// display quiz when user clicks start quiz button
+function buildQuiz(){}
+// display correct/wrong in the footer, 
+function questionResult (){}
+// displays ability for user to save score
+function saveScore () {}
+// surface highscores when "Highscores" nav button is clicked
+function displayScore (){}
 
 //$(".start-button").on("click", funtion(){
 //    setTime();
